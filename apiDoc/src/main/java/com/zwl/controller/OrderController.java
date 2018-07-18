c678c02efb5ac8791a2bfc1522de7182f7932094
weiliangzhou <382308664@qq.com>
2018-07-18 14:18:31 +0800

    修改api文档接口地址

--- apiDoc/src/main/java/com/zwl/controller/OrderController.java
+++ apiDoc/src/main/java/com/zwl/controller/OrderController.java
@@ -24,14 +24,14 @@ import java.util.List;
 public class OrderController {
     @ApiComment("订单列表")
     @RequestMapping(name = "订单列表",
-            value = "/teacher/getOrderList", method = RequestMethod.POST)
+            value = "/teacher/order/getOrderList", method = RequestMethod.POST)
     public OrderVo getOrderList(@ApiComment("商户号") String merchantId, @ApiComment("pageNum") Integer pageNum, @ApiComment("pageSize") Integer pageSize, @ApiComment("手机号") String phone, @ApiComment("订单状态 0未付款 1成功 -1超时关闭") Integer orderStatus) {
         OrderVo orderVo=new OrderVo();
         return orderVo;
     }
     @ApiComment("个人消费列表")
     @RequestMapping(name = "个人消费列表",
-            value = "/teacher/getOrderListByUserId", method = RequestMethod.POST)
+            value = "/teacher/order/getOrderListByUserId", method = RequestMethod.POST)
     public List<Order> getOrderListByUserId(@ApiComment("userId") String userId, @ApiComment("pageNum") Integer pageNum, @ApiComment("pageSize") Integer pageSize) {
         List<Order> orderList = new ArrayList<>();
         return orderList;
