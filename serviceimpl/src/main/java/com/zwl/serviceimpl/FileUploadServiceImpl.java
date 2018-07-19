6ca401fc1c3cfa42c56542bbc273224f9544aab4
weiliangzhou <382308664@qq.com>
2018-07-19 19:02:24 +0800

    更新图片上传

--- serviceimpl/src/main/java/com/zwl/serviceimpl/FileUploadServiceImpl.java
+++ serviceimpl/src/main/java/com/zwl/serviceimpl/FileUploadServiceImpl.java
@@ -4,13 +4,12 @@ import com.zwl.model.exception.BSUtil;
 import com.zwl.service.FileUploadService;
 import com.zwl.util.AliOSSUtil;
 import com.zwl.util.FileCheckUtil;
+import com.zwl.util.UUIDUtil;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.BufferedOutputStream;
 import java.io.File;
-import java.io.FileOutputStream;
 
 /**
  * @author 二师兄超级帅
@@ -22,11 +21,6 @@ import java.io.FileOutputStream;
 @Service
 @Slf4j
 public class FileUploadServiceImpl implements FileUploadService {
-    //    windows
-//    private static String uploadPath = "F:\\sass\\image\\";
-    //    linux
-    private static String uploadPath = "/home/upload";
-
     @Override
     public String upload(MultipartFile file, int type) {
         if (file.isEmpty())
@@ -37,6 +31,8 @@ public class FileUploadServiceImpl implements FileUploadService {
         log.info("ContentType：" + file.getContentType());
         log.info("Name：" + file.getName());
         log.info("Size：" + file.getSize());
+
+
         //TODO:文件大小、名称、类型检查的业务处理
         if (1 == type) {
             if (file.getSize() > 3145728)
@@ -45,29 +41,48 @@ public class FileUploadServiceImpl implements FileUploadService {
             if (!FileCheckUtil.isImageType(file.getContentType()))
                 BSUtil.isTrue(false, "请上传正确的图片类型");
         }
-        try {
-            // 检查上传目录
-            File targetFile = new File(uploadPath);
-            if (!targetFile.exists()) {
-                targetFile.mkdirs();
-            }
 
-            // 实例化输出流
-            BufferedOutputStream out = new BufferedOutputStream(new FileOutputStream(uploadPath + file.getOriginalFilename()));
-            out.write(file.getBytes());
-            out.flush();
-            out.close();
+
+        try {
+            // 获取文件名
+            String fileName = file.getOriginalFilename();
+            // 获取文件后缀
+            String prefix = fileName.substring(fileName.lastIndexOf("."));
+            // 用uuid作为文件名，防止生成的临时文件重复
+            final File excelFile = File.createTempFile(UUIDUtil.getUUID32(), prefix);
+            // MultipartFile to File
+            file.transferTo(excelFile);
+
+            //你的业务逻辑
 
             // 上传到OSS
-            String url = AliOSSUtil.uploadLocalFile(new File(uploadPath + file.getOriginalFilename()), "upload/image/");
+            String url = AliOSSUtil.uploadLocalFile(excelFile, "upload/image/");
             if (url == null)
                 BSUtil.isTrue(false, "上传oss失败");
             log.info("上传完毕,访问地址:" + url);
 
+            //程序结束时，删除临时文件
+            deleteFile(excelFile);
             return url;
+
         } catch (Exception e) {
             e.printStackTrace();
         }
         return null;
     }
+
+
+    /**
+     * 删除
+     *
+     * @param files
+     */
+    private void deleteFile(File... files) {
+        for (File file : files) {
+            if (file.exists()) {
+                file.delete();
+            }
+        }
+    }
+
 }
