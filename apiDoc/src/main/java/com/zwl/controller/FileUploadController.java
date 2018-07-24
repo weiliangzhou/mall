7dc3c142309adfd5d7cc3f73d012513bda1c74df
李子沫 <lixingpeng1990@126.com>
2018-07-24 14:44:28 +0800

    “更新 token拦截

--- apiDoc/src/main/java/com/zwl/controller/FileUploadController.java
+++ apiDoc/src/main/java/com/zwl/controller/FileUploadController.java
@@ -1,10 +1,12 @@
 package com.zwl.controller;
 
 import com.alibaba.fastjson.JSON;
+import com.terran4j.commons.api2doc.annotations.Api2Doc;
 import com.terran4j.commons.api2doc.annotations.ApiComment;
 import com.zwl.model.baseresult.Result;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 /**
@@ -14,7 +16,8 @@ import org.springframework.web.multipart.MultipartFile;
  * @Description: TODO
  * @date 2018/7/2220:35
  */
-@ApiComment("老师后台上传图片")
+@Api2Doc(name = "老师后台上传图片")
+@RestController
 public class FileUploadController {
     @ApiComment("上传图片")
     @RequestMapping(name = "上传图片",
