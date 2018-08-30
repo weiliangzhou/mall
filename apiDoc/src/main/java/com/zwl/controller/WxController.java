fe6b332705bed2b0dc5f5b2effa8c74adfd508f3
陈康钟 <15967160657@163.com>
2018-08-30 17:56:45 +0800

    api

--- apiDoc/src/main/java/com/zwl/controller/WxController.java
+++ apiDoc/src/main/java/com/zwl/controller/WxController.java
@@ -339,7 +339,7 @@ public class WxController {
         return result;
     }
     @ApiComment(value = "新增观后感", seeClass =ClassInfoComment.class)
-    @RequestMapping(name = "新增观后感", value = "/wx/classInfoComment/getClassInfoCommentList", method = RequestMethod.POST)
+    @RequestMapping(name = "新增观后感", value = "/wx/classInfoComment/add", method = RequestMethod.POST)
     public Result addClassInfoComment(@ApiComment("userId") String userId,
                                       @ApiComment("节课id") Long classInfoId,
                                       @ApiComment("微信昵称") String nickname,
