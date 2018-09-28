b8ae14125199e0e2527f0bf51db1c9a94f6160d4
weiliangzhou <382308664@qq.com>
2018-09-28 16:22:45 +0800

    更新

--- apiDoc/src/main/java/com/zwl/controller/OfflineActivityController.java
+++ apiDoc/src/main/java/com/zwl/controller/OfflineActivityController.java
@@ -1,5 +1,6 @@
 package com.zwl.controller;
 
+import com.terran4j.commons.api2doc.annotations.Api2Doc;
 import com.terran4j.commons.api2doc.annotations.ApiComment;
 import com.zwl.model.baseresult.Result;
 import com.zwl.model.vo.OfflineActivityBuy;
@@ -16,8 +17,8 @@ import org.springframework.web.bind.annotation.RestController;
  * @Description: TODO
  * @date 2018/9/2617:14
  */
+@Api2Doc(name = "线下活动")
 @RestController
-@Slf4j
 public class OfflineActivityController {
     @ApiComment(seeClass = WxPayVo.class)
     @RequestMapping(name = "线下报名",
