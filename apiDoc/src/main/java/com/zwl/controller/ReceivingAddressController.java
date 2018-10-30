8b56f92637ecfa9a871245c95ed4d88603505058
weiliangzhou <382308664@qq.com>
2018-10-30 15:01:40 +0800

    账户设置

--- apiDoc/src/main/java/com/zwl/controller/ReceivingAddressController.java
+++ apiDoc/src/main/java/com/zwl/controller/ReceivingAddressController.java
@@ -3,8 +3,9 @@ package com.zwl.controller;
 import com.terran4j.commons.api2doc.annotations.Api2Doc;
 import com.terran4j.commons.api2doc.annotations.ApiComment;
 import com.zwl.model.baseresult.Result;
-import org.springframework.web.bind.annotation.PostMapping;
+import com.zwl.model.po.UserReceivingAddress;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -17,9 +18,11 @@ import org.springframework.web.bind.annotation.RestController;
 @Api2Doc(name = "收货地址管理")
 @RestController
 @RequestMapping("/wx/userReceivingAddress")
+@ApiComment(seeClass = UserReceivingAddress.class)
 public class ReceivingAddressController {
-    @ApiComment("保存城市、性别")
-    @PostMapping("/auth/saveUserReceivingAddress")
+    @ApiComment("保存")
+    @RequestMapping(name = "保存",
+            value = "/auth/saveUserReceivingAddress", method = RequestMethod.POST)
     public Result saveUserInfo(@ApiComment("收货人姓名") String receivingName,
                                @ApiComment("性别0男 1女") String gender,
                                @ApiComment("省") String province,
@@ -31,13 +34,15 @@ public class ReceivingAddressController {
     ) {
         return new Result();
     }
-    @ApiComment("保存城市、性别")
-    @PostMapping("/auth/getUserReceivingAddressList")
-    public Result getUserReceivingAddressList(
+
+    @ApiComment("获取列表")
+    @RequestMapping(name = "获取列表",
+            value = "/auth/getUserReceivingAddressList", method = RequestMethod.POST)
+    public UserReceivingAddress getUserReceivingAddressList(
             @ApiComment("商户号") String merchantId,
             @ApiComment("是否默认地址 0不是 1是") String isDefault
     ) {
-        return new Result();
+        return new UserReceivingAddress();
     }
 
 }
