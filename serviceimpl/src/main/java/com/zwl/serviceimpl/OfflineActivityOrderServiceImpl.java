41db67dbedfd5bfc06c3e37e47177e3e1a54eddd
renpengliang <641780120@qq.com>
2018-11-01 21:39:19 +0800

    去除code

--- serviceimpl/src/main/java/com/zwl/serviceimpl/OfflineActivityOrderServiceImpl.java
+++ serviceimpl/src/main/java/com/zwl/serviceimpl/OfflineActivityOrderServiceImpl.java
@@ -279,7 +279,7 @@ public class OfflineActivityOrderServiceImpl implements OfflineActivityOrderServ
         offlineActivityOrderVo.setSex(offlineActivityOrder.getSex());
         offlineActivityOrderVo.setCity(offlineActivityOrder.getCity());
         offlineActivityOrderVo.setActivityThemeId(offlineActivityOrder.getActivityThemeId());
-        offlineActivityOrderVo.setActivityCode(offlineActivityOrder.getActivityCode());
+        //offlineActivityOrderVo.setActivityCode(offlineActivityOrder.getActivityCode());
         OfflineActivityTheme offlineActivityTheme = offlineActivityThemeService.getOfflineActivityThemeDetailByThemeId(offlineActivityOrder.getMerchantId(), offlineActivityOrder.getActivityThemeId());
         offlineActivityOrderVo.setImgUrl(offlineActivityTheme.getImgUrl());
         offlineActivityOrderVo.setThemeName(offlineActivityTheme.getThemeName());
