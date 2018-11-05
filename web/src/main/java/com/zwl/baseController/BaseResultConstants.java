93e0a88e8e0ed4402e2c45374972aec9369cbd10
weiliangzhou <382308664@qq.com>
2018-11-05 17:00:06 +0800

    规范

--- web/src/main/java/com/zwl/baseController/BaseResultConstants.java
+++ web/src/main/java/com/zwl/baseController/BaseResultConstants.java
@@ -7,8 +7,8 @@ package com.zwl.baseController;
 public class BaseResultConstants {
     public static final String HTTP_CODE_200 = "200";
     public static final String HTTP_CODE_500 = "500";
-    public static final String HTTP_CODE_200_VALUE = "SUCCESS";
-    public static final String HTTP_CODE_500_VALUE = "FAIL";
+    public static final String HTTP_CODE_200_VALUE = "成功";
+    public static final String HTTP_CODE_500_VALUE = "失败";
     public static final String HTTP_CODE_900 = "900";
     public static final String HTTP_CODE_900_VALUE = "token 失效 ";
     public static final String FEGIN_MSG = "大师兄，师傅被妖怪抓走了";
